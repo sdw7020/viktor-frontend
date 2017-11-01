ad68d11ecfde585446033655354699630960045b
Mees van Dijk <mees@itisme.es>
2017-11-01 14:16:24 +0100

    added reactstrap with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './viktorlogo.png';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './viktorlogo.png'
+import './App.css'
+import { Button } from 'reactstrap'
 
 class App extends Component {
   render() {
@@ -10,16 +11,17 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">viktor Admin Panel</h1>
         </header>
-        <p className="App-intro">
-          Welcome to the viktor Admin Panel!
-        </p>
-        <br /><br /><br /><br />
+        <p className="App-intro">Welcome to the viktor Admin Panel!</p>
+        <br />
+        <br />
+        <br />
+        <br />
 
-        <input class="login" type="password" autoFocus></input>
-        <button class="login">Log in</button>
+        <input class="login" type="password" autoFocus />
+        <Button class="login">Log in</Button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-const store = createStore(/* add reducer here*/)
+const store = createStore(() => true /* add reducer here*/)
 
 ReactDOM.render(
   <Provider store={store}>
