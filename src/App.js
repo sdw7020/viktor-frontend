ca05dc2ecf6b5551ea716d18e8a7600aa9f5c318
sdw7020 <dewitte.sytze9@gmail.com>
2017-11-01 14:53:18 +0100

    added .DS_Store to .gitignore

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">viktor Admin Panel</h1>
+          <h1 className="App-title">made by Group 22</h1>
         </header>
         <br />
         <br />
