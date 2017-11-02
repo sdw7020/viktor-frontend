cf8955e565a57c44a2d9266e291bf11be4f92104
sdw7020 <dewitte.sytze9@gmail.com>
2017-11-02 19:49:41 +0100

    added wrong password modal

--- src/components/modals/WrongPasswordModal.js
+++ src/components/modals/WrongPasswordModal.js
@@ -1,8 +1,34 @@
 import React from "react"
-import { UncontrolledAlert } from "reactstrap"
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap"
+import { connect } from "react-redux"
 
-export default () => (
-  <UncontrolledAlert className="wrongPassword" color="danger">
-    Wrong password.
-  </UncontrolledAlert>
+const WrongPasswordModal = ({ hideModal }) => (
+  <Modal isOpen={true}>
+    <ModalHeader>Error</ModalHeader>
+    <ModalBody>Wrong password.</ModalBody>
+    <ModalFooter>
+      <Button color="secondary" onClick={hideModal}>
+        OK
+      </Button>
+    </ModalFooter>
+  </Modal>
 )
+
+const mapStateToProps = state => ({
+  username: state.modal.username,
+})
+
+const mapDispatchToProps = dispatch => ({
+  deleteUser: username =>
+    dispatch({
+      type: "DELETE_USER",
+      username,
+    }),
+  hideModal: () =>
+    dispatch({
+      type: "HIDE_MODAL",
+      modal: "deleteUser",
+    }),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(WrongPasswordModal)
